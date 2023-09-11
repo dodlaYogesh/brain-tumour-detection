aa0b7db0213a10b6e34b089c7e4bcd8849cf2e2b
dodlaYogesh <144482428+dodlaYogesh@users.noreply.github.com>
2023-09-11 21:55:43 +0530

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,12 @@
+fname=input("enter ur first name: ")
+lname=input("enter ur last name: ")
+rno=input("enter ur hall Ticket Number: ")
+college=input('college (Acet/Adtp) :')
+if college.lower() == "acet":
+    code="Acet.ac.in"
+elif college.lower() == "adtp":
+    code="Adtp.edu.in"
+else:
+    print("please enter correct code: ")
+mail=rno+"@"+code
+print(mail)
